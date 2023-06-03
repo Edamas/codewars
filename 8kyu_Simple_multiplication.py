7c30997116696a25e3359d04c5dc9826386ca42a
Elysio Neto <49333427+Edamas@users.noreply.github.com>
2023-06-03 05:10:08 -0300

    Create 8kyu_Simple_multiplication.py
    Simple multiplication
    8 kyu
    Python
    
    DESCRIPTION:
    
    This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
    
    TAGs:
      FUNDAMENTALS

---
+++ 8kyu_Simple_multiplication.py
@@ -0,0 +1,84 @@
+'''Simple multiplication
+8 kyu
+Python
+
+DESCRIPTION:
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+TAGs:
+  FUNDAMENTALS
+'''
+
+# Solution 1
+def simple_multiplication(number) :
+    return number * 8 if number % 2 == 0 else number * 9
+
+
+# Solution 2
+def simple_multiplication(number) :
+    return number * 9 if number % 2 else number * 8
+
+
+# Solution 3
+def simple_multiplication(n) :
+    return n * (8 + n % 2)
+
+
+# Solution 4
+def simple_multiplication(number) :
+    if number % 2 == 0:
+        return number * 8
+    return number * 9
+
+
+# Solution 5
+def simple_multiplication(number) :
+    return number * (8 if number % 2 == 0 else 9)
+
+
+# Solution 6
+simple_multiplication = lambda a : a * (8 + (a & 1))
+
+
+# Solution 7
+def simple_multiplication(number) :
+    if number % 2 == 0:
+        n = 8
+    else:
+        n = 9
+    return number * n
+
+
+# Solution 8
+def simple_multiplication(n):
+    return n * [8, 9][n % 2]
+
+
+# Solution 9
+simple_multiplication = lambda n: n * (8, 9)[n % 2]
+
+
+# Solution 10
+simple_multiplication = lambda n: n.__mul__(n.__and__(bool(n)).__add__(len(__name__)))
+
+
+# Bonus Solution 
+def simple_multiplication(number) :
+    '''
+    ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷
+    ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇
+    ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽
+    ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕
+    ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕
+    ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕
+    ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄
+    ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕
+    ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿
+    ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+    ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟
+    ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠
+    ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙
+    ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣
+    '''
+    return number + number + number + number + number + number + number + number + number if number % 2 else number + number + number + number + number + number + number + number
